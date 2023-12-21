15a4a75bd85b216cfa412f5447935ffc1b7f8e51
LDcourrielleur <93119484+LDcourrielleur@users.noreply.github.com>
2023-12-21 15:19:46 +0100

    Mise à jour boite à idée.

--- chrome/courrielleur/content/courrielleur/cm2Idees.js
+++ chrome/courrielleur/content/courrielleur/cm2Idees.js
@@ -7,26 +7,26 @@ const Cc=Components.classes;
 const Ci=Components.interfaces;
 
 // destinataire du message
-const cm2Idees_to="boite-a-idees-melanie2@developpement-durable.gouv.fr";
+const cm2Idees_to="projet-mce@gendarmerie.interieur.gouv.fr";
 
 
 function Valider(){
-  
+
   let txt=document.getElementById("cm2idees-txt").value;
   if (null==txt || ""==txt){
     alert("La zone de saisie est vide.");
     return;
   }
-  
+
   let bt=document.getElementById("btvalider");
   bt.disabled=true;
-  
+
   window.setCursor("wait");
-  
+
   try{
-  
+
     EnvoieIdee(txt);
-    
+
   } catch(ex){
     window.setCursor("auto");
     CourrielleurTrace("[cm2Idees.js] exception:"+ex);
@@ -34,7 +34,7 @@ function Valider(){
 }
 
 function Annuler(){
-  
+
   window.setCursor("auto");
   window.close();
 }
@@ -42,18 +42,18 @@ function Annuler(){
 
 
 var gIdeesListener={
-  
+
   plustard:false,
-  
+
   QueryInterface: function(iid){
-    
+
     if (iid.equals(Ci.nsIMsgSendListener) ||
         iid.equals(Ci.nsIMsgCopyServiceListener) ||
         iid.equals(Ci.nsISupports))
       return this;
     throw Components.results.NS_NOINTERFACE;
   },
-  
+
   // nsIMsgCopyServiceListener
   OnStartCopy: function() {},
   OnProgress: function(aProgress, aProgressMax) {},
@@ -61,21 +61,21 @@ var gIdeesListener={
   GetMessageId: function(aMessageId) {},
   OnStopCopy: function(aStatus) {
     CourrielleurTrace("[cm2Idees.js] OnStopCopy");
-    
+
     window.setCursor("auto");
 
     if (this.plustard){
       window.close();
       return;
     }
-          
+
     if (0==aStatus){
-      
+
       messageFin();
       window.close();
     }
   },
-  
+
   // nsIMsgSendListener
   onStartSending: function(aMsgID, aMsgSize){
     CourrielleurTrace("[cm2Idees.js] onStartSending");
@@ -86,10 +86,10 @@ var gIdeesListener={
 
   onStopSending: function(aMsgID, aStatus, aMsg, returnFileSpec){
     CourrielleurTrace("[cm2Idees.js] onStopSending aStatus:"+aStatus+" - aMsg:"+aMsg);
-    
+
     if (this.plustard)
       return;
-    
+
     if (0!=aStatus){
       window.setCursor("auto");
       // erreur d'envoi => envoyer plus tard?
@@ -104,18 +104,18 @@ var gIdeesListener={
 
 
 
-// si plustard true => envoi plus tard  
+// si plustard true => envoi plus tard
 function EnvoieIdee(texte, plustard){
-  
+
   CourrielleurTrace("[cm2Idees.js] Envoi du texte:'"+texte+"'");
-  
+
   let compFields=Cc["@mozilla.org/messengercompose/composefields;1"]
                     .createInstance(Ci.nsIMsgCompFields);
   let params=Cc["@mozilla.org/messengercompose/composeparams;1"]
                 .createInstance(Ci.nsIMsgComposeParams);
   params.composeFields=compFields;
   let msgCompose=MailServices.compose.initCompose(params);
-  
+
   let compte=PacomeAuthUtils.GetComptePrincipal();
 
   compFields.from=compte.defaultIdentity.email;
@@ -131,18 +131,18 @@ function EnvoieIdee(texte, plustard){
   converter.charset="UTF-8";
   let corps=converter.ConvertFromUnicode(texte);
   converter.Finish();
-  
+
 
   let msgSend=Cc["@mozilla.org/messengercompose/send;1"]
                 .createInstance(Ci.nsIMsgSend);
-                                
+
   msgSend.createAndSendMessage(null,
                                compte.defaultIdentity,
                                compte.key,
                                compFields,
                                false,
                                false,
-                               (plustard ? Ci.nsIMsgSend.nsMsgQueueForLater : 
+                               (plustard ? Ci.nsIMsgSend.nsMsgQueueForLater :
                                 (Services.io.offline ? Ci.nsIMsgSend.nsMsgQueueForLater : Ci.nsIMsgSend.nsMsgDeliverNow)
                                ),
                                null,
@@ -156,50 +156,50 @@ function EnvoieIdee(texte, plustard){
                                null,
                                "",
                                Ci.nsIMsgCompType.New);
-  
+
 }
 
 function getIdentiteBali(){
-  
+
   let cp=PacomeAuthUtils.GetComptePrincipal();
 
   if (null==cp){
     return null;
   }
-  
+
   return cp.defaultIdentity;
 }
 
 
 function getSujet(){
-  
+
   let ver=Services.prefs.getCharPref("courrielleur.version");
   return "[BoiteAIdees] Courrielleur "+ver;
 }
 
 function messageFin(){
-  
+
   let strBundle=Services.strings.createBundle("chrome://courrielleur/locale/courrielleur.properties");
-              
+
   let titre=strBundle.GetStringFromName("cm2idees-merci-titre");
   let msg=strBundle.GetStringFromName("cm2idees-merci-texte");
-  
+
   Services.prompt.alert(window, titre, msg);
 }
 
 // erreur d'envoi du message
 function OnErreurEnvoi(){
-  
+
   // notification utilisateur
   alert("Erreur d'envoi.\nEnvoi plus tard.");
-  
+
   // envoi plus tard
   var txt=document.getElementById("cm2idees-txt").value;
-  
+
   gIdeesListener.plustard=true;
-  
+
   window.setCursor("wait");
-  
+
   EnvoieIdee(txt, true);
-  
+
 }
